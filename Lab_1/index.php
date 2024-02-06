663fe69330ed387e3fb647cf543d7459ac5f11aa
KaRToSHoW <miha.frolow2017@yandex.ru>
2024-02-06 11:45:01 +0300

    Lab_1

--- Lab_1/index.php
+++ Lab_1/index.php
@@ -7,13 +7,154 @@
 </head>
 <body>
     <header style="display:flex; flex-direction:row;box-sizing:border-box; margin:0;font-size:18px;font-family:monospace;"> 
-        <img style="height: 85px;margin-top:10px;margin-right:50px;" src='http://localhost/server-side_development/Lab_1/image/Logo_Polytech_rus_main.jpg'>
+        <img style="height: 85px;margin-top:10px;margin-right:50px;" src='http://localhost/server-side_development/image/Logo_Polytech_rus_main.jpg'>
         <h1>Hello, World!</h1>
     </header>
     <main>
         <h2 style="font-size:22px;">
             <?php
-            echo "Hello, World! Today: ".date("d.m.y");
+                // $a = 20;
+                // $b = 40;
+                // echo $a+$b;
+                // echo ' < ';
+                // if ($a < $b){
+                //     echo $a*$b;
+                // }
+                // echo '<br> Hello, World! Today: '.date("d.m.y").'<br>';
+                // $a= 1; $b = 3; $c = 4;
+                // switch ($a + $b) {
+                //     case 7:
+                //         echo $d = $c/$a;
+                //         break;
+                //     case 6:
+                //         echo $f = $e/$a + $a;
+                //         break;
+                //     case 1:
+                //         echo '<br>';
+                //         break;
+                //     default:
+                //         echo ok;
+                // }
+
+        //         $i = 2; $s = 0;
+        //         switch ($i) {
+        //             case 1:
+        //                 $s++;
+        //             case 2:
+        //                 $s++;
+        //         }
+        //         echo 'S ='.$s.'<br>';
+
+                // $sum = 0;
+                // for ($i = 2; $i < 6; $i++){
+                //     $sum++;
+                //     echo 'S ='.$sum.'<br>';
+                // }
+                // echo 'Sum ='.$sum.'<br>';
+
+
+
+                // $i = 0; $s = 0;
+                // while ($i <5){
+                //     $s++;
+                //     $i++;
+                // }
+                //  echo 'Sum ='.$s.'<br>';
+
+                // $i = 0; $s = 0;
+                // do {
+                //     $s+=$i;
+                //     $i++;
+                // }while ($i < 5);
+                // echo 'Sum ='.$s.'<br>';
+
+                // $k = 0;
+                // $arr = array(3,4,1,13,6,31);
+                // for ($i = 0; $i < 6; $i++){
+                //     $arr[$i]--;
+                //     if(!$arr[$i]) continue;
+                //     $k++;
+                // }
+                // echo 'k ='.$k.'<br>';
+
+                // 1
+                // $a = 27;
+                // $b = 12;
+                // $c = round(sqrt((27**2)+(12**2)), 2);
+                // echo $c;
+
+                // 5 and 7
+                // $a = 2; 
+                // $b = 2.0;
+                // $c = '2';
+                // $d = 'two'; 
+                // $g = true; 
+                // $f = false;
+                // echo '<br>'.gettype($b+$c);
+
+                // 14
+                // $quieter = 'Тише'; 
+                // $go = 'едешь'; 
+                // $further = 'дальше';
+                // echo '<br>'.$quieter.' '.$go.' '.$further.' будешь';
+
+
+                // 27
+                // <table>
+                //     <tr>Выражение</tr>
+                //     <tr>Результат сравнения</tr>
+                //     <?php
+                //     $a = 2; $b = '2'; $d = '2a';
+                //         echo '<td>'.$a.' == '.$b.'</td>';
+                //         echo '<td>'.$a == $b.'</td>';
+                //         echo $a == $b;
+
+
+                // 31
+                // $a = 2; 
+                // $b = 2.0;
+                // $c = '2';
+                // $d = 'two';
+                // $g = true;
+                // $f = false;
+                // echo 'Исходный тип '.gettype($a). ' Полученное значение'.(int)$a.'<br>';
+
+                //35
+                // $a = 36;
+                // $b = '4';
+                // echo ($a%$b >0) ? gettype($a/$b).' '.$a5%$b : $a.' / '.$b.' = '.$a/$b;
+                // echo '<br>'
+
+                //39
+                // $a = 27;
+                // $b = 12;
+                // if ($a) echo $b.'/'.$a.' = '.$b/$a;
+                // else echo (bool)$a;
+
+                // 44
+                // $year = 2022; $month = 3; $day = 2;
+                // echo 'Дата: '.sprintf("%04d-%02d-%02d",$year, $month,$day);
+
+                // 48
+                // $sum = 0;
+                // for ($i=0; $i < 6; $i++) { 
+                //     $sum+=$1;
+                // }
+                // echo $sum;
+
+                // 50
+                // $i = 1;
+                // $sum = 0;
+                // do{
+                //     if ($i%2 == 0){
+                //         $sum+=$i;
+                //         echo $i.'<br>';
+                //     }
+                //     $i++;
+                // }while($i < 21);
+                // echo $sum;
+                        
+                        
             ?>
         </h2>
     </main>
