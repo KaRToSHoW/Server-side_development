f459f8bf669799023885150347ea8c0a80c42587
KaRToSHoW <miha.frolow2017@yandex.ru>
2024-02-15 20:29:11 +0300

    Lab_2 and Lab_3

--- Lab_2/index.php
+++ Lab_2/index.php
@@ -1,50 +1,38 @@
-<?php
-    function separator(){
-        echo '<br>------------<br>';
-    }
-    $m1 = array('summer');
-    print $m1[0].'<br>';
-    separator();
-    $m2[0] = 'winter';
-    var_dump($m2).'<br>';
-
-    $m3 = array(3, 'autumn',100);
-    $m3[3]=7;
-    $m3[5]='five';
-    $m3[6]=22;
-    $m3['arr']='arr';
-    var_dump($m3);
-    separator();
-
-    // for ($i=0; $i < count($m3); $i++) { 
-    //     echo $m3[$i].'<br>';
-    // }
-    foreach($m3 as $value){
-        echo $value.'<br>';
-    }
-    separator();
-    foreach($m3 as $key=>$value){
-        echo $key.'=>'.$value.'<br>';
-    }
-    separator();
-    $a = array(1,2);
-    $b = array(1,3);
-    $c = $a+$b;
-    var_dump($c);
-    echo '<br>';
-    foreach ($c as $key => $value) {
-        echo $key.'='.$value.'<br>';
-    }
-    separator();
-    $d = array(2 => 'a', 3 => 'b', 8 => 'c');
-    var_dump($a+$d);
-    separator();
-    var_dump($a==$b);
-    echo '<br>';
-    var_dump($a===$b);
-    echo '<br>';
-    var_dump($a!=$b);
-    echo '<br>';
-    var_dump($a!==$b);
-    separator();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <?php
+            $int = "10 + x = 33";
+            $elem = explode(" ", $int);
+            $firstpart = $elem[0];
+            $x = $elem[2];
+            $actual_x = 0;
+            $res = $elem[4];
+            switch ($elem[1]) {
+                case "+":
+                    $actual_x = $res - $firstpart;
+                    break;
+                case "-":
+                    $actual_x = $firstpart - $res;
+                    break;
+                case "/":
+                    $actual_x = $firstpart / $res;
+                    break;
+                case "*":
+                    $actual_x = $res / $firstpart;
+                    break;
+                case "**":
+                    $actual_x = log($res, $firstpart);
+                    break;
+            }
+            echo "$actual_x"
+        ?>
+    </main>
+</body>
+</html>
