07f40e0a51c23da23a0e003e5ad7a1aee0f5df3d
KaRToSHoW <miha.frolow2017@yandex.ru>
2024-03-26 09:49:25 +0300

    add pages

--- Lab_6/header.php
+++ Lab_6/header.php
@@ -14,6 +14,7 @@
             font-weight: bold;
             color: blue;
         }
+        
     </style>
 </head>
 
--- Lab_6/view.php
+++ Lab_6/view.php
@@ -1,9 +1,18 @@
 <?php
-    if (isset($_GET['o'])){
-        $sql = 'SELECT * FROM `friends` ORDER BY `'.$_GET['o'].'`';
-    }else $sql ='SELECT * FROM `friends`' ;
+    if (!isset($_GET['o'])) $_GET['o'] ='id';
+    
+    $pages = 5;
+    $page = isset($_GET['page']) && is_numeric($_GET['page']) ? $_GET['page'] : 1;
+    $offset_page = ($page - 1) * $pages;
+    
+    $sql = 'SELECT * FROM `friends` ORDER BY `' . $_GET['o'] . '` LIMIT ' . $offset_page . ', ' . $pages;
     $res = mysqli_query($connect, $sql);
-    if (mysqli_errno($connect)) print_r(mysqli_stmt_error($connect));
+    if (mysqli_errno($connect)) print_r(mysqli_error($connect));
+    
+    $row = mysqli_num_rows($res);
+    $total_rows_query = mysqli_query($connect, 'SELECT COUNT(*) as total FROM `friends`');
+    $total_rows = mysqli_fetch_assoc($total_rows_query)['total'];
+    $total_pages = ceil($total_rows / $pages);
 ?>
 
 <table class="table">
@@ -22,19 +31,26 @@
         </tr>
     </thead>
     <tbody>
-    <?php while($row = mysqli_fetch_assoc($res)):?>
-        <tr>
-            <th scope="row"><?=$row['id'];?></th>
-            <td><?=$row['firstname'];?></td>
-            <td><?=$row['name'];?></td>
-            <td><?=$row['lastname'];?></td>
-            <td><?=$row['gender'];?></td>
-            <td><?=$row['date'];?></td>
-            <td><?=$row['phone'];?></td>
-            <td><?=$row['email'];?></td>
-            <td><?=$row['adress'];?></td>
-            <td><?=$row['comment'];?></td>
-        </tr>
-        <?php endwhile;?>
+        <?php while ($row = mysqli_fetch_assoc($res)) : ?>
+            <tr>
+                <th scope="row"><?= $row['id']; ?></th>
+                <td><?= $row['firstname']; ?></td>
+                <td><?= $row['name']; ?></td>
+                <td><?= $row['lastname']; ?></td>
+                <td><?= $row['gender']; ?></td>
+                <td><?= $row['date']; ?></td>
+                <td><?= $row['phone']; ?></td>
+                <td><?= $row['email']; ?></td>
+                <td><?= $row['adress']; ?></td>
+                <td><?= $row['comment']; ?></td>
+            </tr>
+        <?php endwhile; ?>
     </tbody>
 </table>
+<nav aria-label="...">
+    <ul class="pagination pagination-sm">
+        <?php for($i = 1; $i <= $total_pages; $i++):?>
+            <li class="page-item <?= $page == $i ? 'active' : '' ?>"><a class="page-link" href="<?=$_SERVER['SCRIPT_NAME'];?>?page=<?=$i;?>"><?=$i?></a></li>
+        <?php endfor;?>
+    </ul>
+</nav>
